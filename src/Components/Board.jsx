1d611ff5f7ee86341a922525bf9857671dcf737b
Saniya Mansuri <saniamansuri452@gmail.com>
2023-07-08 09:55:41 +0530

    added the players with value x and o

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -3,17 +3,24 @@ import Square from './Square';
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setisXNext] = useState(false);
 
   console.log(squares);
   const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition]) {
+      return;
+    }
+
     setSquares(currentSquares => {
       return currentSquares.map((squareValue, position) => {
         if (clickedPosition === position) {
-          return 'X';
+          return isXNext ? 'X' : 'O';
         }
+
         return squareValue;
       });
     });
+    setisXNext(currentisXNext => !currentisXNext);
   };
 
   const renderSquare = position => {
